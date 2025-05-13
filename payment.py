a476854e2475d4c867432f6e79a4922666af8b8c
x7mdx <uae14w1@gmail.com>
2025-05-13 23:53:29 +0530

    Added docs to payment.py

--- payment.py
+++ payment.py
@@ -2,12 +2,18 @@ from enum import Enum
 from purchaseorder import OrderStatus
 
 class PaymentMethod(Enum):
+    """ Different Types of Payment Method """
     CREDIT_CARD = "Credit Card"
     DEBIT_CARD = "Debit Card"
     DIGITAL_WALLET = "Digital Wallet"
 
 
 class Payment:
+    """Class Represent a Payment"""
+
+    """
+    A Constructor for Payment class
+    """
     def __init__(self, payment_id, order, amount, payment_method, payment_date):
         self.payment_id = payment_id
         self.order = order  
@@ -15,6 +21,10 @@ class Payment:
         self.payment_method = payment_method  
         self.payment_date = payment_date  
 
+
+    """
+    method to process the payment.
+    """
     def process_payment(self):
         print(f"Processing payment of {self.amount} for Order ID: {self.order.order_id}")
         self.order.update_status(OrderStatus.CONFIRMED)
